e5de3953a4fc4f6cdc5c04bf6fb67d530dd1053c
Tom Close <tom.g.close@gmail.com>
2025-01-04 23:14:35 +1100

    fixed up unittest

--- pydra/tasks/dcm2niix/tests/test_utils.py
+++ pydra/tasks/dcm2niix/tests/test_utils.py
@@ -1,9 +1,10 @@
 from pydra.tasks.dcm2niix.utils import Dcm2Niix
+from fileformats.medimage import DicomDir
 
 
 def test_dcm2niix():
     task = Dcm2Niix()
-    task.inputs.in_dir = "test-data/test_dicoms"
+    task.inputs.in_dir = DicomDir.mock("test-data/test_dicoms")
     task.inputs.out_dir = "test-data"
     task.inputs.compress = "y"
     assert (
